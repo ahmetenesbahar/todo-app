297d75b50c7aaf712ba7d968413e4dd1c53dd65c
ahmetenesbahar <enesbahar2303@gmail.com>
2023-03-16 21:20:58 +0300

    feat: list item styling

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function TodoList() {
-  return <div className=""></div>;
+  return (
+    <div className="todo-list">
+      <div className="card-list">
+        <div className="list-item">
+          <input type="checkbox" />
+          <p className="item-text">abc</p>
+          <div className="item-btn">
+            <button className="edit">Edit</button>
+            <button className="delete">Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TodoList;
